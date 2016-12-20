d8d840b9b38deaafef52127009d91301bc1abae4
Rafael PacMe <rafy.pachecomendez@gmail.com>
2016-12-20 12:46:57 -0300

    Alpha 1.6: Add Mouselib support on Explorer add Beta Support for Mouselib (Thanks to DevDavisNunez for help) Signed by: @ROM-PacMe

--- explorer.lua
+++ explorer.lua
@@ -18,53 +18,23 @@ if buttons.triangle then
     dofile("../../start_screen.lua")
 end
 
+-- ## ELEMENTO CURSOR (SOLO PSP) ##
+if buttons.held.up then y -= v
+elseif buttons.held.down then y += v
+elseif buttons.held.left then x -= v
+elseif buttons.held.right then x += v
+end
+
+image.blit(cursor, x, y) -- Dibujamos el cursor
+	
 -- ## BARRA DE ESTADO ##
 screen.print(0,0,""..wlan.over(),0.6)
 screen.print(370,0,""..batt.lifepercent().." %",0.6)
 screen.print(415,0,os.date("%I:%M %p"),0.6)
 
-screen.print(35,17,"Ajustes",0.6)
-screen.print(35,29,"Música",0.6)
-screen.print(35,41,"Fotos",0.6)
-screen.print(35,53,"Videos",0.6)
-screen.print(35,65,"Mensajes",0.6)
-screen.print(35,77,"Usuario",0.6)
-screen.print(35,89,"Apagar",0.6)
-screen.print(12,y," -> ",0.6)
-
-if buttons.down then
-    option += 1
-    y += 12
-elseif buttons.up then
-    option -= 1
-    y -= 12
-end
-if option < 1 then
-    option,y = 7,89
-end
-if option > 7 then
-    option,y = 1,17
-end
-
-if buttons.cross then
-    if option == 1 then dofile("../ajustes.lua")
-    elseif option == 2 then dofile("../musica.lua")
-    elseif option == 3 then dofile("../fotos.lua")
-    elseif option == 4 then dofile("../videos.lua")
-    elseif option == 5 then dofile("../mensajes.lua")
-    elseif option == 6 then dofile("../usuario.lua")
-    elseif option == 7 then 
-    answer = os.message("Seguro que deseas apagar?",1)
-        if answer == 0 then
-            os.message("gracias!")
-        else
-            os.exit()
-        end
-    end
-end
 
 -- ## ELEMENTOS / OBJETOS SELECCIONABLES ##
-screen.print(78,122,"Esta pantalla es solo de prueba para la Alpha. \n                                (Alpha 1.5-2)")
+screen.print(78,122,"Esta pantalla es solo de prueba para la Alpha. \n                                (Alpha 1.6)")
 
 screen.flip()
 end
